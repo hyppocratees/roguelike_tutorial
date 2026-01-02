42b64528180ce7d12b16690e69368d65a15c1d21
hyppocratees <hyppocratees@gmail.com>
2026-01-02 22:22:13 +0100

    added missing new file

---
+++ src/item.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include "components/consumable.h"
+
+#include "item.h"
+
+std::ostream& operator<<(std::ostream& os, const Item& item) {
+	os << item.name_ << "\n";
+	os << item.char_ << "\n";
+	os << item.x_ << " " << item.y_ << "\n";
+	os << item.block_mov_ << "\n";
+	if (item.consumable_) {
+		os << "1" << "\n";
+		os << *item.consumable_ << "\n";
+	}
+	else {
+		os << "0" << "\n";
+	}
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, Item& item) {
+	is >> std::ws;
+	std::getline(is, item.name_);
+	is >> item.char_;
+	is >> item.x_ >> item.y_;
+	is >> item.block_mov_;
+	bool has_consumable = false;
+	is >> has_consumable;
+	if (has_consumable) {
+		int cons_type;
+		int var1;
+		int var2;
+		is >> cons_type;
+		switch (cons_type) {
+		case 0:
+			item.consumable_ = nullptr;
+			break;
+		case 1:
+			is >> var1;
+			item.consumable_ = std::make_unique<HealingConsumable>(var1);
+			break;
+		case 2:
+			is >> var1 >> var2;
+			item.consumable_ = std::make_unique<FireballConsumable>(var1, var2);
+			break;
+		case 3:
+			is >> var1 >> var2;
+			item.consumable_ = std::make_unique<LightningDamageConsumable>(var1, var2);
+			break;
+		case 4:
+			is >> var1;
+			item.consumable_ = std::make_unique<ConfusionConsumable>(var1);
+			break;
+		default:
+			item.consumable_ = nullptr;
+		}
+	}
+
+
+	return is;
+}
